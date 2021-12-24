466402278066f166743f9f669f1e4ccb41fd2a7b
Chbig <bigo_charly@hotmail.fr>
2021-12-24 18:25:43 +0100

    TEST

--- chain.js
+++ chain.js
@@ -1,55 +1,75 @@
 const Block = require("./block");
+const Libp2p = require("libp2p");
+const TCP = require("libp2p-tcp");
+const { NOISE } = require("libp2p-noise");
+const MPLEX = require("libp2p-mplex");
+const process = require("process");
 
 class BlockChain {
-    constructor() {
-        this.blockchain = [this.startGenesisBlock()];
-    }
-    startGenesisBlock() {
-        return new Block({});
-    }
-    obtainLatestBlock() {
-        return this.blockchain[this.blockchain.length - 1];
-    }
+  constructor() {
+    this.blockchain = [this.startGenesisBlock()];
 
-    // Ajoute un nouveau bloque dans la chaine si celui-ci est valide
-    addNewBlock(newBlock) { 
-        // TODO : comment certifier que le bloque est valide ? Consensus ?
+    // TODO : récupérer la liste des noeuds du réseau depuis le noeud d'amorcage
 
-        newBlock.prevHash = this.obtainLatestBlock().hash; // On ajoute la signature du block précédent au nouveau block
-        newBlock.hash = newBlock.computeHash(); // On calcul la signature du nouveau bloque
-        this.blockchain.push(newBlock); // On ajoute le nouveau bloque à la chaine
+    // On crée le noeud sur le réseau
+    this.node = await Libp2p.create({
+      addresses: {
+        // add a listen address (localhost) to accept TCP connections on a random port
+        listen: ["/ip4/127.0.0.1/tcp/0"],
+      },
+      modules: {
+        transport: [TCP],
+        connEncryption: [NOISE],
+        streamMuxer: [MPLEX],
+      },
+    });
 
-        // TODO : communiquer avec les autres noeux sur l'ajout du bloque
+    // TODO : On communique aux autres noeuds l'adresse du noeud
+  }
+
+  startGenesisBlock() {
+    return new Block({});
+  }
+  obtainLatestBlock() {
+    return this.blockchain[this.blockchain.length - 1];
+  }
+
+  // Ajoute un nouveau bloque dans la chaine si celui-ci est valide
+  addNewBlock(newBlock) {
+    // TODO : comment certifier que le bloque est valide ? Consensus ?
+
+    newBlock.prevHash = this.obtainLatestBlock().hash; // On ajoute la signature du block précédent au nouveau block
+    newBlock.hash = newBlock.computeHash(); // On calcul la signature du nouveau bloque
+    this.blockchain.push(newBlock); // On ajoute le nouveau bloque à la chaine
+
+    // TODO : communiquer avec les autres noeux sur l'ajout du bloque
+  }
+
+  // On vérifie la validité de toute la chaine
+  checkChainValidity() {
+    for (let i = 1; i < this.blockchain.length; i++) {
+      const currBlock = this.blockchain[i];
+      const prevBlock = this.blockchain[i - 1];
+
+      // On vérifie la signature su bloque courrant
+      if (currBlock.hash !== currBlock.computeHash()) {
+        return false;
+      }
+
+      // On vérifie la signature du block précédent
+      if (currBlock.prevHash !== prevBlock.hash) {
+        return false;
+      }
     }
-   
-    // On vérifie la validité de toute la chaine
-    checkChainValidity() { 
-        for(let i = 1; i < this.blockchain.length; i++) {
-            const currBlock = this.blockchain[i];
-            const prevBlock = this.blockchain[i -1];
-            
-            // On vérifie la signature su bloque courrant
-            if(currBlock.hash !== currBlock.computeHash()) { 
-                return false;
-            }
-          
-            // On vérifie la signature du block précédent
-            if(currBlock.prevHash !== prevBlock.hash) {                 
-              return false;
-            }
-            
-        }
-        return true; // Tous les bloques de la chaine sont valide
-    }
+    return true; // Tous les bloques de la chaine sont valide
+  }
 }
 
+let a = new Block({ from: "Joe", to: "Jane" });
+let b = new Block({ from: "Jane", to: "Joe" });
 
-let a = new Block({from: "Joe", to: "Jane"});
-let b = new Block({from: "Jane", to: "Joe"});
-
- 
 let chain = new BlockChain();
 chain.addNewBlock(a);
 chain.addNewBlock(b);
-console.log(chain); 
+console.log(chain);
 console.log("Validity: " + chain.checkChainValidity());
--- test.js
+++ test.js
@@ -1,65 +1,59 @@
-const Libp2p = require('libp2p')
-const TCP = require('libp2p-tcp')
-const { NOISE } = require('libp2p-noise')
-const MPLEX = require('libp2p-mplex')
-const process = require('process')
+const Libp2p = require("libp2p");
+const TCP = require("libp2p-tcp");
+const { NOISE } = require("libp2p-noise");
+const MPLEX = require("libp2p-mplex");
+const process = require("process");
 
 const main = async () => {
-    const node = await Libp2p.create({
-        addresses: {
-        // add a listen address (localhost) to accept TCP connections on a random port
-        listen: ['/ip4/127.0.0.1/tcp/0']
-        },
-        modules: {
-        transport: [TCP],
-        connEncryption: [NOISE],
-        streamMuxer: [MPLEX]
-        }
-    })
-    
-    // start libp2p
-    await node.start()
-    console.log('libp2p has started')
-    
-    // print out listening addresses
-    console.log('listening on addresses:')
-    node.multiaddrs.forEach(addr => {
-        console.log(`${addr.toString()}/p2p/${node.peerId.toB58String()}`)
-    })
-    
-    // ping peer if received multiaddr
-    if (process.argv.length >= 3) {
-        const ma = process.argv[2]
-        console.log(`pinging remote peer at ${process.argv[2]}`)
-        const latency = await node.ping(ma)
-        console.log(`pinged ${process.argv[2]} in ${latency}ms`)
+  const node = await Libp2p.create({
+    addresses: {
+      // add a listen address (localhost) to accept TCP connections on a random port
+      listen: ["/ip4/127.0.0.1/tcp/0"],
+    },
+    modules: {
+      transport: [TCP],
+      connEncryption: [NOISE],
+      streamMuxer: [MPLEX],
+    },
+  });
 
-        
-    } else {
-        console.log('no remote peer address given, skipping ping')
-    }
+  // start libp2p
+  await node.start();
+  console.log("libp2p has started");
 
-      
-      node.connectionManager.on('peer:connect', (connection) => {
-        console.log('Connected to %s', connection.remotePeer.toB58String()) // Log connected peer
-      });
+  // print out listening addresses
+  console.log("listening on addresses:");
+  node.multiaddrs.forEach((addr) => {
+    console.log(`${addr.toString()}/p2p/${node.peerId.toB58String()}`);
+  });
 
-      node.connectionManager.on('peer:disconnect', (connection) => {
-        console.log('Disconnected from %s', connection.remotePeer.toB58String()) // Log connected peer
-      })
+  // ping peer if received multiaddr
+  if (process.argv.length >= 3) {
+    const ma = process.argv[2];
+    console.log(`pinging remote peer at ${process.argv[2]}`);
+    const latency = await node.ping(ma);
+    console.log(`pinged ${process.argv[2]} in ${latency}ms`);
+  } else {
+    console.log("no remote peer address given, skipping ping");
+  }
 
-    
-    const stop = async () => {
-        // stop libp2p
-        await node.stop()
-        console.log('libp2p has stopped')
-        process.exit(0)
-    }
-    
-    process.on('SIGTERM', stop)
-    process.on('SIGINT', stop)
-}
+  node.connectionManager.on("peer:connect", (connection) => {
+    console.log("Connected to %s", connection.remotePeer.toB58String()); // Log connected peer
+  });
 
-main()
+  node.connectionManager.on("peer:disconnect", (connection) => {
+    console.log("Disconnected from %s", connection.remotePeer.toB58String()); // Log connected peer
+  });
 
+  const stop = async () => {
+    // stop libp2p
+    await node.stop();
+    console.log("libp2p has stopped");
+    process.exit(0);
+  };
 
+  process.on("SIGTERM", stop);
+  process.on("SIGINT", stop);
+};
+
+main();
